fa3e99d8f8d537577850c0f603edf6495b5cf794
Saurav Shrestha <sauravshrestha2005@gmail.com>
2024-06-26 16:11:04 +0545

    change data

--- src/db/data.js
+++ src/db/data.js
@@ -1,61 +1,71 @@
 export const data = [{
-    "id": 1,
-    "img": "http://dummyimage.com/145x100.png/ff4444/ffffff",
-    "time": "7/21/2023",
-    "title": "Junior Executive",
-    "desc": "Oth injury of right pulmonary blood vessels, subs encntr"
-  }, {
-    "id": 2,
-    "img": "http://dummyimage.com/171x100.png/5fa2dd/ffffff",
-    "time": "11/2/2023",
-    "title": "Account Representative IV",
-    "desc": "Epidural hemorrhage w LOC >24 hr w ret consc lev"
-  }, {
-    "id": 3,
-    "img": "http://dummyimage.com/100x100.png/ff4444/ffffff",
-    "time": "2/15/2024",
-    "title": "Chief Design Engineer",
-    "desc": "Ped on sktbrd injured in clsn w 2/3-whl mv nontraf, sequela"
-  }, {
-    "id": 4,
-    "img": "http://dummyimage.com/157x100.png/ff4444/ffffff",
-    "time": "8/10/2023",
-    "title": "Structural Engineer",
-    "desc": "Oth fracture of shaft of right humerus, init for clos fx"
-  }, {
-    "id": 5,
-    "img": "http://dummyimage.com/136x100.png/ff4444/ffffff",
-    "time": "12/12/2023",
-    "title": "Accounting Assistant III",
-    "desc": "Other ice-skates accident"
-  }, {
-    "id": 6,
-    "img": "http://dummyimage.com/212x100.png/dddddd/000000",
-    "time": "8/26/2023",
-    "title": "Human Resources Assistant II",
-    "desc": "Fx unsp metatarsal bone(s), r foot, subs for fx w nonunion"
-  }, {
-    "id": 7,
-    "img": "http://dummyimage.com/229x100.png/cc0000/ffffff",
-    "time": "12/13/2023",
-    "title": "Food Chemist",
-    "desc": "Other recurrent vertebral dislocation, cervical region"
-  }, {
-    "id": 8,
-    "img": "http://dummyimage.com/166x100.png/5fa2dd/ffffff",
-    "time": "11/27/2023",
-    "title": "Staff Scientist",
-    "desc": "Disp fx of epiphy (separation) (upper) of unsp femur, sqla"
-  }, {
-    "id": 9,
-    "img": "http://dummyimage.com/210x100.png/5fa2dd/ffffff",
-    "time": "9/23/2023",
-    "title": "Senior Cost Accountant",
-    "desc": "Flexion deformity"
-  }, {
-    "id": 10,
-    "img": "http://dummyimage.com/212x100.png/5fa2dd/ffffff",
-    "time": "12/19/2023",
-    "title": "Safety Technician IV",
-    "desc": "Disp fx of dist phalanx of l idx fngr, 7thD"
-  }]
+  "id": 1,
+  "img": "http://dummyimage.com/125x100.png/cc0000/ffffff",
+  "date": "5/8/2024",
+  "time": "7:40 AM",
+  "title": "Nurse Practicioner",
+  "desc": "Other ossification of muscle, other site"
+}, {
+  "id": 2,
+  "img": "http://dummyimage.com/241x100.png/dddddd/000000",
+  "date": "4/2/2024",
+  "time": "3:53 AM",
+  "title": "Engineer IV",
+  "desc": "Insect bite (nonvenomous) of right little finger"
+}, {
+  "id": 3,
+  "img": "http://dummyimage.com/249x100.png/cc0000/ffffff",
+  "date": "4/5/2024",
+  "time": "5:34 AM",
+  "title": "Media Manager IV",
+  "desc": "Inj unsp blood vessel at wrist and hand of left arm, subs"
+}, {
+  "id": 4,
+  "img": "http://dummyimage.com/169x100.png/ff4444/ffffff",
+  "date": "4/16/2024",
+  "time": "8:55 PM",
+  "title": "Technical Writer",
+  "desc": "Foreign body in right ear, subsequent encounter"
+}, {
+  "id": 5,
+  "img": "http://dummyimage.com/249x100.png/cc0000/ffffff",
+  "date": "1/22/2024",
+  "time": "2:44 PM",
+  "title": "Cost Accountant",
+  "desc": "Other chronic suppurative otitis media, left ear"
+}, {
+  "id": 6,
+  "img": "http://dummyimage.com/143x100.png/dddddd/000000",
+  "date": "2/16/2024",
+  "time": "12:07 PM",
+  "title": "Financial Analyst",
+  "desc": "Stiffness of left knee, not elsewhere classified"
+}, {
+  "id": 7,
+  "img": "http://dummyimage.com/134x100.png/5fa2dd/ffffff",
+  "date": "2/9/2024",
+  "time": "5:38 PM",
+  "title": "Software Consultant",
+  "desc": "Laceration with foreign body of left forearm"
+}, {
+  "id": 8,
+  "img": "http://dummyimage.com/107x100.png/ff4444/ffffff",
+  "date": "1/31/2024",
+  "time": "3:41 PM",
+  "title": "Technical Writer",
+  "desc": "Underdosing of antacids and anti-gstrc-sec drugs, init"
+}, {
+  "id": 9,
+  "img": "http://dummyimage.com/213x100.png/ff4444/ffffff",
+  "date": "4/15/2024",
+  "time": "10:58 AM",
+  "title": "Developer III",
+  "desc": "Ped on sktbrd injured in clsn w rail trn/veh in traf, init"
+}, {
+  "id": 10,
+  "img": "http://dummyimage.com/110x100.png/cc0000/ffffff",
+  "date": "5/26/2024",
+  "time": "11:22 PM",
+  "title": "Staff Scientist",
+  "desc": "Mtrcy passenger injured in clsn w ped/anml in traf, init"
+}]
