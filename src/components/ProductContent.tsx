930c29a40a105ab19f6ad99bfc1b1ec9d4cd4ac1
Saurav Shrestha <sauravshrestha2005@gmail.com>
2024-06-26 16:30:22 +0545

    data

--- src/components/ProductContent.tsx
+++ src/components/ProductContent.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { data } from '@/db/data';
+import React from "react";
+import { data } from "@/db/data";
 
 export default function ProductContent() {
   return (
     <div>
-        
+      {data.map((item, index) => (
+        <div key={index} className="col-span-3 grid grid-cols-1">
+          <div className="flex">
+            <img src={item.img} alt={item.title} className="w-40 h-40" />
+            <div>
+              <h1 className="text-2xl font-semibold">{item.title}</h1>
+              <p className="text-textclr_green">{item.date}</p>
+              <p>{item.time}</p>
+              <p>{item.desc}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
