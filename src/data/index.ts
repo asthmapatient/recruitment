0cfdc1c9181b7e13041e2cf1e20d307c4f26c39a
Aadil Shrestha <aadilshrestha12345@gmail.com>
2024-06-26 14:35:07 +0545

    added dummy data

---
+++ src/data/index.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    title: "Herald DevCorps Communities",
+    datePosted: "2021-09-01",
+    deadline: "2021-09-30",
+    timing: "Full-time",
+    recuirmentFor: "student",
+  },
+  {
+    title: "Facilities Management",
+    datePosted: "2021-09-01",
+    deadline: "2021-09-30",
+    timing: "Full-time",
+    recuirmentFor: "outside",
+  },
+  {
+    title: "Accountancy & Finance Sector",
+    datePosted: "2021-09-01",
+    deadline: "2021-09-30",
+    timing: "Part-time",
+    recuirmentFor: "outside",
+  },
+  {
+    title: "IT Sector",
+    datePosted: "2021-09-01",
+    deadline: "2021-09-30",
+    timing: "Full-time",
+    recuirmentFor: "student",
+  },
+];
